acd756f59e66fdaef28cdf2fda235fc11662d6c2
Nam Pham <nam.pham@adnovum.vn>
2025-09-07 13:14:15 +0700

    Init project

---
+++ weather app uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather app uikit
+//
+//  Created by Nam Pham on 7/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weather app uikitTests/weather_app_uikitTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_uikitTests.swift
+//  weather app uikitTests
+//
+//  Created by Nam Pham on 7/9/25.
+//
+
+import Testing
+@testable import weather_app_uikit
+
+struct weather_app_uikitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
