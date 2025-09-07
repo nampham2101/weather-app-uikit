d1822ded7ab60f5f7099c4d551d2718de8f4f2b9
Nam Pham <nam.pham@adnovum.vn>
2025-09-07 14:39:19 +0700

    Added controller for details view

---
+++ weather app uikit/DetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.swift
+//  weather app uikit
+//
+//  Created by Nam Pham on 7/9/25.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Details"
+    }
+}
