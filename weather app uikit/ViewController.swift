8969748fed4d6b7b4a4b83d4d651efea96332af3
Nam Pham <nam.pham@adnovum.vn>
2025-09-14 10:48:49 +0700

    Show weather condition and its icon

--- weather app uikit/ViewController.swift
+++ weather app uikit/ViewController.swift
@@ -11,6 +11,9 @@ import CoreLocation
 class ViewController: UIViewController {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    @IBOutlet weak var iconImg: UIImageView!
     
     let WEATHER_API_KEY: String = "347b203f58ab437cb9c115827251309"
     
@@ -42,14 +45,35 @@ class ViewController: UIViewController {
                 self.cityLabel.text = "\(_city) city"
             }
             
-            if let _current = json["current"] as? [String: Any],
-               let _temp_c = _current["temp_c"] as? Double{
-                self.temperatureLabel.text = "\(_temp_c) °C"
+            if let _current = json["current"] as? [String: Any] {
+                if let _temp_c = _current["temp_c"] as? Double {
+                    self.temperatureLabel.text = "\(_temp_c) °C"
+                }
+                
+                if let _condition = _current["condition"] as? [String: Any],
+                   let _text = _condition["text"] as? String,
+                   let _icon = _condition["icon"] as? String {
+                    self.conditionLabel.text = _text
+                    
+                    let _url = URL(string: "https:\(_icon)")!
+                    self.loadIcon(fromUrl: _url)
+                }
             }
         } catch {
             print("Error: \(error)")
         }
     }
 
+    func loadIcon(fromUrl: URL) {
+        URLSession.shared.dataTask(with: fromUrl) { data, response, error in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.iconImg.image = image
+                }
+            } else {
+                print("Failed to load image", error ?? "Uknown error")
+            }
+        }.resume()
+    }
 }
 
